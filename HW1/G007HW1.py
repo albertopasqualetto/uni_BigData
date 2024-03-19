8b2f68168285764512e6be7fb30417a033e0f16c
Alberto Pasqualetto <alberto.pasqualetto.2@studenti.unipd.it>
2024-03-19 11:45:32 +0100

    HW1 boilerplate

---
+++ HW1/G007HW1.py
@@ -0,0 +1,15 @@
+from pyspark import SparkContext, SparkConf
+
+def main():
+    conf = SparkConf().setAppName('G007HW1')
+    sc = SparkContext(conf=conf)
+    pass
+
+def ExactOutliers():
+    pass
+
+def ApproxOutliers():
+    pass
+
+if __name__ == "__main__":
+    main()
