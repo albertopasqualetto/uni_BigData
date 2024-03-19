d065ba78c7b9d50c8198c9e4a6fba483f5ee6373
Alberto Pasqualetto <alberto.pasqualetto.2@studenti.unipd.it>
2024-03-19 14:31:17 +0100

    Read file to RDD

--- HW1/G007HW1.py
+++ HW1/G007HW1.py
@@ -3,6 +3,12 @@ from pyspark import SparkContext, SparkConf
 def main():
     conf = SparkConf().setAppName('G007HW1')
     sc = SparkContext(conf=conf)
+
+    docs = sc.textFile('TestN15-input.txt')\
+                                    .flatMap(lambda s: [tuple(float(x) for x in s.split(','))])\
+                                    .cache()
+    print(docs.count())
+    print(docs.collect())
     pass
 
 def ExactOutliers():
