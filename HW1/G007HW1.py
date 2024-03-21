431b14f51f50d1470da131e22c3fefd8e90c2fc7
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2024-03-21 10:31:35 +0100

    Added if with 200000

--- HW1/G007HW1.py
+++ HW1/G007HW1.py
@@ -7,8 +7,17 @@ def main():
     docs = sc.textFile('TestN15-input.txt')\
                                     .flatMap(lambda s: [tuple(float(x) for x in s.split(','))])\
                                     .cache()
-    print(docs.count())
+    num = docs.count()
+    print()
     print(docs.collect())
+    if num < 200000:
+        print('ExactOutliers')
+        ExactOutliers()
+        print('ApproxOutliers')
+        ApproxOutliers()
+    else:
+        print('ApproxOutliers')
+        ApproxOutliers()
     pass
 
 def ExactOutliers():
