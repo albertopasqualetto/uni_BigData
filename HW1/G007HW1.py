a8cf49f10cb8011f6f9f427546a25ef67b82db6c
Michela Schibuola <michela.schibuola@gmail.com>
2024-03-21 15:07:13 +0100

    roundB_3 and roundB_7

--- HW1/G007HW1.py
+++ HW1/G007HW1.py
@@ -52,14 +52,32 @@ def ApproxOutliers(points, D, M):
 
 def roundA(points, D):
     return points.flatmap(lambda x, y: ((int(x/D), int(y/D)), 1)).reduceByKey(lambda x, y: x+y) # count the number of points in each cell
+    
+def cell_mapping(cell, square_dim): 
+    squares = []
+    #range: from -1 to 1 if square_dim = 3, from  -3 to 3 if square_dim = 7
+    for i in range(-int(square_dim/2), int(square_dim/2)): 
+        for j in range (-int(square_dim/2), int(square_dim/2)):
+            if(i == 0 and j == 0): #if the current cell is the center
+                squares.append((cell[0] + i, cell[1] + j), (cell[2], 1)) #((q_x, q_y), (|L_j|, 1))
+            else:
+                squares.append((cell[0], cell[1]), (cell[2], 0)) #((q_x, q_y), (|L_j|, 0))
+    return squares
 
+def square_reduce(square):
+    center_count = 0
+    points_count = 0 #count of the points in the square
+    for cell in square:    
+        points_count += cell[2] #update the points count
+        center_count += cell[3] #if there is a center 1 is added 
+    if center_count != 0: #if the center cell has at least one point
+        return points_count
 
 def roundB_3(points_per_cell):
-    pass
-
+    return points_per_cell.flatmap(lambda cell: cell_mapping(cell, 3)).reduceByKey(square_reduce)
 
 def roundB_7(points_per_cell):
-    pass
+    return points_per_cell.flatmap(lambda cell: cell_mapping(cell, 7)).reduceByKey(square_reduce)
 
 
 def roundC(cells, M):
