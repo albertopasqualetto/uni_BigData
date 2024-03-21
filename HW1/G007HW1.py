ee4807fc3b0abc091b23b5996a2990f0ba5b7dda
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2024-03-21 10:54:53 +0100

    Add first round and input

--- HW1/G007HW1.py
+++ HW1/G007HW1.py
@@ -1,30 +1,48 @@
 from pyspark import SparkContext, SparkConf
+import sys
 
 def main():
     conf = SparkConf().setAppName('G007HW1')
     sc = SparkContext(conf=conf)
-
-    docs = sc.textFile('TestN15-input.txt')\
+    argc = len(sys.argv)
+    if argc != 5:
+        print('Usage: python G007HW1.py <file_name> <D> <M> <K> <L>')
+        exit(1)
+    file_name = sys.argv[1]
+    D = int(sys.argv[2])
+    M = int(sys.argv[3])
+    K = int(sys.argv[4])
+    L = int(sys.argv[5])
+    points = sc.textFile(file_name)\
                                     .flatMap(lambda s: [tuple(float(x) for x in s.split(','))])\
+                                    .repartition(L)\
                                     .cache()
-    num = docs.count()
-    print()
-    print(docs.collect())
+    num = points.count()
+    print(num)
+    print(points.collect())
     if num < 200000:
         print('ExactOutliers')
-        ExactOutliers()
+        ExactOutliers(points, D)
         print('ApproxOutliers')
-        ApproxOutliers()
+        ApproxOutliers(points, D)
     else:
         print('ApproxOutliers')
-        ApproxOutliers()
+        ApproxOutliers(points)
     pass
 
-def ExactOutliers():
-    pass
+def ExactOutliers(points, D):
+    pointsList = points.collect()
+    # TODO: Implement the exact algorithm for finding outliers
+    return pointsList
 
-def ApproxOutliers():
-    pass
+def ApproxOutliers(points, D):
+    points = round1(points, D)
+    #TODO: Implement the other rounds of the approximate algorithm
+    return points
+
+def round1(points, D):
+    return points.flatmap(lambda x,y : ((x/D,y/D),1)).reduceByKey(lambda x,y : x+y)  
+    
 
 if __name__ == "__main__":
     main()
