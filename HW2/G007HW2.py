298753236e6ce551fb29d76ced1a6a826479f103
Alberto Pasqualetto <alberto.pasqualetto.2@studenti.unipd.it>
2024-04-23 10:49:56 +0200

    Starting point

--- HW2/G007HW2.py
+++ HW2/G007HW2.py
@@ -3,11 +3,17 @@ import sys
 import time
 import math
 
+conf = SparkConf().setAppName('G007HW2')
+sc = SparkContext(conf=conf)
+sc.setLogLevel("WARN")
+
+C = None    # set of centers
+
 
 def main():
     argc = len(sys.argv)
     if argc != 5:
-        print('Usage: python3 G007HW1.py <file_name> <M> <K> <L>')
+        print('Usage: python3 G007HW2.py <file_name> <M> <K> <L>')
         sys.exit(1)
     file_name = sys.argv[1]
     M = int(sys.argv[2])    # threshold for outliers
@@ -15,10 +21,6 @@ def main():
     L = int(sys.argv[4])    # number of partitions
 
     print(f"{file_name} M={M} K={K} L={L}")
-
-    conf = SparkConf().setAppName('G007HW1')
-    sc = SparkContext(conf=conf)
-    sc.setLogLevel("WARN")
 
     # import file into an RDD of strings (rawData)
     rawData = sc.textFile(file_name)
@@ -32,11 +34,41 @@ def main():
     num = inputPoints.count()
     print("Number of inputPoints =", num)
 
+    MRFFT(inputPoints, K)
 
-    # APPROXIMATE ALGORITHM
-    # MRApproxOutliers(inputPoints, D, M, K)
 
-    # plot_points(inputPoints.collect(), D)
+def SequentialFFT(P, K):
+    # P is the list of points
+    # K is the number of clusters
+    # returns a set C of K centers
+    # O(|P|*K)
+    pass
+
+
+def MRFFT(P, K):
+    # P is the list of points
+    # K is the number of clusters
+    # D is the radius (float)
+    pass
+
+
+def FFTround1():
+    # compute the coreset
+    pass
+
+
+def FFTround2():
+    # obtain the centers from SequentialFFT
+    global C    # set of centers
+    C = sc.broadcast([1, 2, 3, 4, 5])
+    pass
+
+
+def FFTround3():
+    # compute the radius R (float)
+    global C
+    C.value
+    pass
 
 
 def MRApproxOutliers(inputPoints, D, M):
@@ -55,53 +87,53 @@ def MRApproxOutliers(inputPoints, D, M):
 
 
 def ApproxOutliersAlgo(points, M, D):
-    points_per_cell = roundA(points, D).cache() # cache because after all the rounds we have to reuse this RDD
-    points_square_3 = roundB_3(points_per_cell)
-    points_square_7 = roundB_7(points_per_cell)
+    points_per_cell = AOroundA(points, D).cache() # cache because after all the rounds we have to reuse this RDD
+    points_square_3 = AOroundB_3(points_per_cell)
+    points_square_7 = AOroundB_7(points_per_cell)
 
     # merge all the obtained results
     u = points_square_3.union(points_square_7).union(points_per_cell)
     u = u.groupByKey()\
                      .map(lambda x: (x[0], tuple(x[1])))
 
-    outliers = roundC(u, M)
+    outliers = AOroundC(u, M)
     return (points_per_cell, outliers.collect())
 
 
 # from the set of points of the dataset, returns the cells of the grid and for each cell how many points are inside
-def roundA(points, D):
+def AOroundA(points, D):
     return points\
-                .map(lambda pt: map_roundA(pt, D))\
+                .map(lambda pt: AOmap_roundA(pt, D))\
                 .reduceByKey(lambda val1, val2: val1+val2)  # map a point into its cells and then counts the number of points inside each cell
 
 
 # from the set of cells, returns the squares 3x3 with the number of points inside
-def roundB_3(points_per_cell):
+def AOroundB_3(points_per_cell):
     return points_per_cell\
-                        .flatMap(lambda cs: map_roundB(cs, 3))\
-                        .reduceByKey(reduce_roundB)
+                        .flatMap(lambda cs: AOmap_roundB(cs, 3))\
+                        .reduceByKey(AOreduce_roundB)
 
 
 # from the set of cells, returns the squares 7x7 with the number of points inside
-def roundB_7(points_per_cell):
+def AOroundB_7(points_per_cell):
     return points_per_cell\
-                        .flatMap(lambda cs: map_roundB(cs, 7))\
-                        .reduceByKey(reduce_roundB)
+                        .flatMap(lambda cs: AOmap_roundB(cs, 7))\
+                        .reduceByKey(AOreduce_roundB)
 
 
 # from the points per cell, points per 3x3 square and point per 7x7 square, 
 # returns an RDD with "outliers", "non-outliers" and "uncertain" as key and with the list of cells of that type 
 # and the number of points for each category
 # 'empty' is used when the center cell of the square is empty
-def roundC(cells, M):
+def AOroundC(cells, M):
     return cells\
-                .map(lambda cs: map_roundC(cs, M))\
+                .map(lambda cs: AOmap_roundC(cs, M))\
                 .groupByKey()\
-                .flatMap(reduce_roundC)
+                .flatMap(AOreduce_roundC)
 
 
 # map each point into the cell it belongs, with value 1
-def map_roundA(point, D):
+def AOmap_roundA(point, D):
     # point = (x1, y1)
     SIDE = D/(2*(2**0.5))
     # if x or y is negative the starting index is -1, while 0 for the positive values
@@ -117,7 +149,7 @@ def map_roundA(point, D):
 
 
 # map each cell C into a list of squares such that they all contain the cell C
-def map_roundB(cell, square_dim):
+def AOmap_roundB(cell, square_dim):
     # cell = [(i1, j1), # of points in (i1, j1)]
     squares_cells = []
     for i in range(-int(square_dim/2), int(square_dim/2) + 1):
@@ -130,7 +162,7 @@ def map_roundB(cell, square_dim):
 
 
 # returns the number of points in the square, and 1 if the center cell contains points, 0 otherwise
-def reduce_roundB(square1, square2):
+def AOreduce_roundB(square1, square2):
     # square = (# of points in the square, 0 or 1)
     points_count = 0    # count of the points in the square
     center_count = 0    # count of valid centers (will result 1 only if the center cell contains points)
@@ -141,7 +173,7 @@ def reduce_roundB(square1, square2):
 
 # map all the information into a pair with identifier ("outliers", "non-outliers", "uncertain", "empty") and the value is 
 # the index of the cell that contain that type of points and the number of points it contains
-def map_roundC(cell, M):
+def AOmap_roundC(cell, M):
     # cell = [(i, j), [(points_count3, center_count3), (points_count7, center_count7), # of points in (i,j)]]
     if cell[1][0][1] != 0:  # the zero means that the square is built around an empty cell
         N3 = cell[1][0][0]
@@ -159,7 +191,7 @@ def map_roundC(cell, M):
 # returns an RDD with "outliers", "non-outliers", and "uncertain" as key and with the list of cells of that type 
 # and the number of points for each category
 # 'empty' is used for the center cell of the square that is empty
-def reduce_roundC(cells):
+def AOreduce_roundC(cells):
     # cells = [outliers/non-outliers/uncertain/empty, [((i1, j1), # of points in (i1, j1)), ((i2, j2), # of points in (i2, j2)), ...]]
     list_square = []
     number_of_points = 0
@@ -167,25 +199,6 @@ def reduce_roundC(cells):
         list_square.append(cell[0])
         number_of_points += cell[1]
     return (cells[0], (list_square, number_of_points))
-
-
-# (not requested, used for debugging) plot the points, the grid with squares of diagonal D/2 and the ball of each point
-def plot_points(points_list, D):
-    from matplotlib import pyplot as plt
-    import numpy as np
-    fig, ax = plt.subplots()
-    for p in points_list:
-        ax.add_patch(plt.Circle(p, D, color='k', fill=False))
-    x = tuple(p[0] for p in points_list)
-    y = tuple(p[1] for p in points_list)
-    plt.scatter(x, y)
-    for i in range(len(points_list)):
-        plt.annotate(str(i), (x[i], y[i]))
-    SIDE = D/(2*(2**0.5))
-    ax.set_xticks(np.arange(0, max(x)+SIDE, SIDE))
-    ax.set_yticks(np.arange(0, max(y)+SIDE, SIDE))
-    ax.grid(which='both')
-    plt.show()
 
 
 if __name__ == "__main__":
